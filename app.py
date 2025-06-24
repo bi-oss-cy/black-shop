10d29d49e3ba4721f9bac2bb08b8bceb59b2bb1c
bi-oss-cy <fjfj3521@gmail.com>
2025-06-24 23:34:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -105,4 +105,6 @@ dashboard_template = """
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
